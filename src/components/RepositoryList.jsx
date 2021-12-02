904dfdf1c42d9fc1f308869de43396d4fc6d6f80
Rodrigo Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-12-02 18:55:09 -0300

    API github data fetching

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/components/c-repository.scss';
@@ -9,6 +10,16 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/rodrigoanastacio/repos')
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
+  console.log(repositories);
+
   return (
     <section className="c-repository">
       <h1 className="c-repository__title">Listagem de repositórios</h1>
