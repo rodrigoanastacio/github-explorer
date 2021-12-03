66b8b3e380931c6212dcbe039780857d89ad0a6a
Rodrigo Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-12-03 00:30:09 -0300

    Add typescript to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,17 +1,27 @@
+import React from 'react';
+
 import '../styles/components/c-repository-item.scss';
 
-export function RepositoryItem({ repository }) {
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
   return (
     <li className="c-repository-item">
       <strong className="c-repository-item__title">
-        {repository?.name ?? 'Default'}
+        {props.repository?.name ?? 'Default'}
       </strong>
       <p className="c-repository-item__description">
-        {repository?.description}
+        {props.repository?.description}
       </p>
       <a
         target="_blank"
-        href={repository?.html_url}
+        href={props.repository?.html_url}
         className="c-repository-item__link"
       >
         Acessar repositório
